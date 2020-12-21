a5ca043d8ad4f44b5f8e866f088894e19fbf376d
anahir21 <an211291@hotmail.com>
2020-12-21 13:44:49 -0600

    actualizacion 21121

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,55 @@
 const cipher = {
-  // ...
-};
+  encode: function (offset, texto){
 
+    var resultado = "";
+    if (offset === null){
+      throw TypeError ('No es válido');
+    
+    } else if (texto == []){
+      throw new TypeError ('No es válido');  
+    }
+ else if (offset === 0){
+   throw new TypeError ('No es válido');
+ }
+
+    for (let i=0; i <texto.length; i++){
+       let newtext = texto.charCodeAt(i); //charCodeAt convertirá cada una de mis letras a ASCII
+    if (newtext >= 65 && newtext <= 90){
+     resultado += String.fromCharCode((newtext - 65 + offset) % 26 + 65);}//regresará si está en mayusculas
+    else if (newtext >=97 && newtext <=122){
+    resultado += String.fromCharCode((newtext - 97 + offset) % 26 + 97);} //regresará cifrado si está en minusculas
+    else {
+     resultado += texto.charAt(i);//
+    }
+  }
+return resultado;
+    }, 
+
+    decode: function (offset, texto){
+      var resultado2 = "";
+      var offset2= 26-(offset%26);
+      if (offset === null){
+        throw TypeError ('No es válido');
+      
+      } else if (texto == []){
+        throw new TypeError ('No es válido');  
+      }
+   else if (offset === 0){
+     throw new TypeError ('No es válido');
+   }
+
+      for (let i=0; i <texto.length; i++){
+     
+      let newtext = texto.charCodeAt(i);
+      if (newtext >= 65 && newtext <= 90){
+       resultado2 += String.fromCharCode((newtext - 65 + offset2) % 26 + 65);}
+      else if (newtext >=97 && newtext <=122){
+      resultado2 += String.fromCharCode((newtext - 97 + offset2) % 26 + 97);}
+      else {
+       resultado2 += texto.charAt(i);
+      }
+    }
+  return resultado2;
+  },
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
-
 console.log(cipher);
+
+
+
+//los métodos addevent.listener y document.getelementbyid jalan los valores del html
+//los valores de la función para cifrar quedarían así
+document.getElementById("btn-cifrar").addEventListener ("click", function(){
+let texto = document.getElementById("texto1").value;
+let desplazamiento = document.getElementById("desplazamiento").value;
+let offset= parseInt(desplazamiento);
+document.getElementById("texto2").innerHTML = cipher.encode(offset, texto);
+});
+//las valores (letiables) se estarían tomando de esta manera
+document.getElementById("btn-descifrar").addEventListener ("click", function(){
+let texto = document.getElementById("texto1").value;
+let desplazamiento = document.getElementById("desplazamiento").value;
+let offset= parseInt(desplazamiento);
+document.getElementById("texto2").innerHTML = cipher.decode(offset, texto);
+});
+document.getElementById("welcome").addEventListener("click");
